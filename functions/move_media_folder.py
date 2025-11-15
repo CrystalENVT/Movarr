8b8c61a8d9974da13ef71a0e964d63e112fa9f5f
CrystalENVT <email@crystal.envt.gay>
2025-11-15 15:42:53 -0600

    add tests for move_media_folder

--- functions/move_media_folder.py
+++ functions/move_media_folder.py
@@ -4,7 +4,7 @@ import shutil
 def move_media_folder(current_working_dir: str, media_type: str, media_is_anime: bool):
     output_directory = ''
     if media_is_anime == True:
-        output_directory = os.environ('anime_{}_output_directory').replace('~',os.getenv('HOME'))
+        output_directory = os.getenv('anime_{}_output_directory'.format(media_type)).replace('~',os.getenv('HOME'))
     else:
-        output_directory = os.environ('{}_output_directory').replace('~',os.getenv('HOME'))
+        output_directory = os.getenv('{}_output_directory'.format(media_type)).replace('~',os.getenv('HOME'))
     shutil.move(current_working_dir, output_directory)
---
+++ tests/test_move_media_folder.py
@@ -0,0 +1,50 @@
+
+# Needed to be able to import from functions
+import sys
+sys.path.append('./functions')
+
+# function to test
+import move_media_folder
+
+# additional external libraries
+import os
+
+# Test library
+import pytest
+
+@pytest.mark.parametrize(
+        'given_media_type,given_media_is_anime',
+        [
+            (
+                'movie',
+                False
+            ),
+            (
+                'tv',
+                False
+            ),
+            (
+                'movie',
+                True
+            ),
+            (
+                'tv',
+                True
+            ),
+        ]
+)
+def test_move_media_folder(given_media_type: str, given_media_is_anime: bool, mocker):
+
+    # Load fake paths into environment variables
+    os.environ['anime_{}_output_directory'.format(given_media_type)] = 'shutil'
+    os.environ['{}_output_directory'.format(given_media_type)] = 'shutil'
+
+    # mock shutil
+    mocked_shutil = mocker.patch('move_media_folder.shutil.move')
+
+    move_media_folder.move_media_folder(
+        given_media_type,
+        given_media_type,
+        given_media_is_anime
+        )
+    mocked_shutil.assert_called_with(given_media_type, 'shutil')
